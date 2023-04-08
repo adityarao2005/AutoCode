2af1aa5de08f7f156578ff07d9e5fde79a7f70ad
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-04-08 19:39:31 -0400

    Describes this project

---
+++ AutoCore/src/com/raos/autocode/core/package-info.java
@@ -0,0 +1,13 @@
+package com.raos.autocode.core;
+
+
+/*
+ * This is the AutoCore tester area
+ * This framework will have basic utilities:
+ * - Basic algorithms and Data structures
+ * - Basic Design patterns and interfaces
+ * - Wrapping IO
+ * - Basic Reflection
+ * - Creating Properties and Pseudo-Objects
+ * - Creating Object Templates
+ */
