b56095a55c101ac4781c1be06edc329dba0eb71c
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-08-17 15:03:50 -0400

    Added AutoDeep for competitive programmers.. got all the classes here

---
+++ AutoDeep/src/main/java/com/raos/autocode/deep/ds/DisjointSet.java
@@ -0,0 +1,32 @@
+package com.raos.autocode.deep.ds;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.raos.autocode.deep.algorithm.UnionFind;
+
+// Represents the Disjoint set data structure
+// Houses a bunch of sets in the forms of trees and is used mostly for mst
+public class DisjointSet<E> implements UnionFind<E> {
+	// Fields used to store the respective values
+	private Map<E, E> parents = new HashMap<>();
+	private Map<E, Integer> ranks = new HashMap<>();
+	private Map<E, Integer> sizes = new HashMap<>();
+
+	// Overrides the methods to return the fields
+	@Override
+	public Map<E, E> parents() {
+		return parents;
+	}
+
+	@Override
+	public Map<E, Integer> ranks() {
+		return ranks;
+	}
+
+	@Override
+	public Map<E, Integer> sizes() {
+		return sizes;
+	}
+
+}
