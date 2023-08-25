be4ccf8f731153e2adeadfd4d081828ec7a50b92
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-08-25 12:39:48 -0400

    Added a few utility classes.. this is gonna be unfinished for a bit

---
+++ AutoCore/src/main/java/com/raos/autocode/core/util/reflection/ObjectSpy.java
@@ -0,0 +1,8 @@
+package com.raos.autocode.core.util.reflection;
+
+import com.raos.autocode.core.annotations.ClassPreamble;
+
+@ClassPreamble(author = "Aditya Rao", date = "8/25/2023")
+public class ObjectSpy {
+
+}
